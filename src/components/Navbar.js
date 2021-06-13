3f2ae33aa9f27d5528ca1f9f7b1fd03810540b2d
Korneliu <olbiton@gmail.com>
2021-06-12 23:01:01 -0700

    installed react-redux, created actions and reducers folder

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => (
   <div>
     <h2>Navbar</h2>
     <NavLink to='/' activeClassName="is-active" exact={true}>MySpace</NavLink>
-    <NavLink to='/notes' activeClassName='is-active ' >Notes</NavLink>
+    <NavLink to='/notes' activeClassName='is-active'>Notes</NavLink>
     <NavLink to='/code' activeClassName='is-active'>Code</NavLink>
   </div>
 );
