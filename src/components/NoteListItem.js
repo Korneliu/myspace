f1b670b42e7f35a682f49a50cebf91c19676e015
Korneliu <olbiton@gmail.com>
2021-06-22 23:14:51 -0700

    added remove button to NoteListItem

--- src/components/NoteListItem.js
+++ src/components/NoteListItem.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeNote } from '../actions/notes';
 
 
-
-const NoteListItem = ({ description, note }) => (
+const NoteListItem = ({ dispatch, id, description, note }) => (
   <div>
     <h3>{description}</h3>
     <p>{note}</p>
+    <button onClick={() => {
+      dispatch(removeNote({ id }))
+    }}>Remove</button>
   </div>
-)
+);
 
-
-export default NoteListItem;
+export default connect()(NoteListItem);
