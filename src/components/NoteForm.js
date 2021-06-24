f44abd3c191e4bc4c5c339288f2691e548b03dba
Korneliu <olbiton@gmail.com>
2021-06-23 23:25:11 -0700

    added NoteForm component

---
+++ src/components/NoteForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class NoteForm extends React.Component {
+  render() {
+    return (
+      <div>
+        NoteForm
+      </div>
+    )
+  }
+}
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import NoteListItem from './NoteListItem';
+import NoteForm from './NoteForm';
 
 const Notes = (props) => (
   <div>
+    <h1>Add Note</h1>
+    <NoteForm />
     <h1>Notes List</h1>
     {
       props.notes.map((note) => {
         return <NoteListItem key={note.id}{...note} />
       })
     }
+
   </div>
 )
 
