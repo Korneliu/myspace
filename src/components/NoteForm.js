60807f8219c416d589f2f5799b8815063abada26
Korneliu <olbiton@gmail.com>
2021-06-23 23:36:36 -0700

    added NoteForm

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -1,10 +1,36 @@
 import React from 'react';
 
+
 export default class NoteForm extends React.Component {
+  state = {
+    description: '',
+    note: ''
+  }
+  onDescriptionChange = (e) => {
+    const description = e.target.value;
+    this.setState(() => ({ description }))
+  };
+  onNoteChange = (e) => {
+    const note = e.target.value;
+    this.setState(() => ({ note }))
+  }
   render() {
     return (
       <div>
-        NoteForm
+        <form>
+          <input
+            type="text"
+            placeholder="Description"
+            autoFocus
+            value={this.state.description}
+            onChange={this.onDescriptionChange}
+          />
+          <textarea placeholder="Add your note"
+            onChange={this.onNoteChange}
+          >
+          </textarea>
+          <button>Add Note</button>
+        </form>
       </div>
     )
   }
