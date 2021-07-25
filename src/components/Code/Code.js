39ebb30e75cf9dccb1c7476feaac3eb494cf1ca2
Korneliu <olbiton@gmail.com>
2021-07-24 19:24:53 -0700

    added link in code and games page

--- src/components/Code/Code.js
+++ src/components/Code/Code.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Code = () => (
   <div>
     <h1>Work in progress...</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Go to Main Page</NavLink>
   </div>
 );
 
--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../Navbar';
+import { NavLink } from 'react-router-dom';
 
 const Games = () => (
   <div>
-    <Navbar />
     <h1>Work in progress...</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Go to Main Page</NavLink>
+
   </div>
 );
 
