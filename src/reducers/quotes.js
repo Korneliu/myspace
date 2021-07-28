a09b712b5af017c581f45150f1c5ecd720dad062
Korneliu <olbiton@gmail.com>
2021-07-27 23:13:55 -0700

    added ADD_QUOTE action

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,11 @@
 
 const quotesDefaultState = [];
-
+export default (state = quotesDefaultState, action) => {
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return [
+        ...state,
+        action.quote
+      ]
+  }
+}
