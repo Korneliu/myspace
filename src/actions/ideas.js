54bf598a07783e28de2b578245b55e8685a4abc6
Korneliu <olbiton@gmail.com>
2021-08-11 22:51:38 -0700

    replaced few Ideas in database

--- src/actions/ideas.js
+++ src/actions/ideas.js
@@ -42,7 +42,6 @@ export const startSetIdeas = () => {
           ...childSnapshot.val()
         })
       });
-      console.log(ideas[0].title);
       dispatch(setIdeas(ideas));
     })
   }
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,7 +14,20 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+
 const database = firebase.database();
+
+database.ref()
+  .once('value')
+  .then((snapshot) => {
+    const val = snapshot.val();
+    console.log(val);
+  })
+  .catch((e) => {
+    console.log('Error fetching data, e')
+  })
+
+
 
 export { firebase, database as default };
 
@@ -25,66 +38,6 @@ export { firebase, database as default };
 
 
 
-
-
-
-
-
-
-
-
-
-/* //child_removed
-database.ref('ideas').on('child_removed', (snapshot) => {
-  console.log(snapshot.key, snapshot.val());
-})
-
-// child_changed
-database.ref('ideas').on('child_changed', (snapshot) => {
-  console.log(snapshot.key, snapshot.val());
-})
-
-// child_added
-database.ref('ides').on('child_added', (snapshot) => {
-  console.log(snapshot.key, snapshot.val());
-});
- */
-
-// database.ref('ideas')
-//   .on('value', (snapshot) => {
-//     const ideas = [];
-//     snapshot.forEach((childSnapshot) => {
-//       ideas.push({
-//         id: childSnapshot.key,
-//         ...childSnapshot.val()
-//       })
-//     })
-//     console.log(ideas)
-//   });
-
-
-// database.ref('ideas').push({
-//   title: 'first idea',
-//   text: 'first idea text'
-// })
-
-
-
-// database.ref()
-//   .on('value', (snapshot) => {
-//     console.log(snapshot.val());
-//   });
-// database.ref('age').set(44)
-
-
-// database.ref('location/city')
-//   .once('value')
-//   .then((snapshot) => {
-//     const val = snapshot.val();
-//     console.log(val);
-//   }).catch((e) => {
-//     console.log('error fetching data', e);
-//   });
 
 
 
