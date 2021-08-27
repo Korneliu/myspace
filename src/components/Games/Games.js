a00c069796de906ceb1afc8dc80a8e904114e84b
Korneliu <olbiton@gmail.com>
2021-08-26 22:53:58 -0700

    converter Games to class component

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Navbar from '../Navbar';
 import { NavLink } from 'react-router-dom';
 
-const Games = () => (
-  <div>
-    <h1>Work in progress...</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Go to Main Page</NavLink>
+export default class Games extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Work in progress...</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Go to Main Page</NavLink>
 
-  </div>
-);
+      </div>
+    )
+  }
 
-export default Games;
+}
+
