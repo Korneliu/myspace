3c6c2863dd28285d67f76e163bab3b17074ee84c
Korneliu <olbiton@gmail.com>
2021-09-03 00:01:45 -0700

    added local storage to Games component

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -6,18 +6,26 @@ import Quotes from './Quotes';
 
 
 export default class Games extends React.Component {
-  state = {
-    randomNumber: 0,
-    number: 0,
-    quotes: ["whatever", "here", "there"],
-    oneQuote: ''
+  constructor(props) {
+    super(props)
+    this.state = JSON.parse(window.localStorage.getItem('state')) || {
+      quotes: ["whatever", "whatevertwo", "whateverthree"],
+      oneQuote: ""
+    }
+    console.log(this.state)
+  }
+  setState(state) {
+    window.localStorage.setItem('state', JSON.stringify(state));
+    super.setState(state);
   }
   generateRandomQuote = e => {
     e.preventDefault();
+    // let randomQuote = this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)]
     this.setState({
       oneQuote: this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)]
     })
     console.log(this.state.oneQuote)
+
   }
   generateRandomNumber = e => {
     e.preventDefault()
@@ -44,4 +52,46 @@ export default class Games extends React.Component {
     )
   }
 };
+
+
+// export default class Games extends React.Component {
+//   state = {
+//     randomNumber: 0,
+//     number: 0,
+//     quotes: ["whatever", "here", "there"],
+//     oneQuote: ''
+//   }
+//   generateRandomQuote = e => {
+//     e.preventDefault();
+//     this.setState({
+//       oneQuote: this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)]
+//     })
+//     console.log(this.state.oneQuote)
+//   }
+//   generateRandomNumber = e => {
+//     e.preventDefault()
+//     this.setState({
+//       number: this.state.number = Math.floor(Math.random() * 11)
+//     })
+//   }
+//   render() {
+//     return (
+//       <div className="games-layout">
+//         <h1>Games, work in progres!</h1>
+//         <Navbar />
+//         <NavLink to="/" activeClassName="is-active" exact={true}>Go to Main Page</NavLink>
+//         <Number
+//           randomNumber={this.generateRandomNumber}
+//           number={this.state.number}
+//         />
+//         <Quotes
+//           randomQuote={this.generateRandomQuote}
+//           quotes={this.state.quotes}
+//           oneQuote={this.state.oneQuote}
+//         />
+//       </div>
+//     )
+//   }
+// };
+
 
--- src/components/Games/Quotes.js
+++ src/components/Games/Quotes.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Quotes = props => (
   <div>
     <h1>Quotes</h1>
-    <button onClick={props.randomQuote}> Genrate Random Quote</button>
+    <button onClick={props.randomQuote}> Generate Random Quote</button>
     <h3>{props.oneQuote}</h3>
 
   </div>
